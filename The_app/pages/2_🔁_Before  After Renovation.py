d38a759d745758db4651b1a13f31724d2e084ce6
HibaDoi <doihiba@iav.ac.ma>
2025-06-16 10:26:31 +0100

    change name for Before / After Renovation debugging___

--- The_app/pages/2_🔁_Before  After Renovation.py
+++ The_app/pages/2_🔁_Before  After Renovation.py
@@ -30,10 +30,12 @@ bucket = client.bucket("renodat")
 # blob = bucket.blob(file_path)
 # st.write("blooooooooooob:", blob)
 
-# List all files in the bucket
-st.write("All files in bucket:")
-for blob in bucket.list_blobs():
-    st.write(f"- {blob.name}")
+# Access the correct file path
+blob = bucket.blob("shpp/u.shp")
+
+# Now it should exist
+if blob.exists():
+    st.write("File found!")
 
 def load_shapefile_from_gcs():
     """Load shapefile directly from Google Cloud Storage using Streamlit secrets"""
